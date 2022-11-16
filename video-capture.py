b1ff8f4e6de6fc1e8f1315b070912e3d46d91540
litscham <108621172+litscham@users.noreply.github.com>
2022-11-16 10:55:34 +0100

    Create video-capture.py

---
+++ video-capture.py
@@ -0,0 +1,19 @@
+#live view with video capture
+video = cv2.VideoCapture(0)
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+size = (frame_width, frame_height)
+result=cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'MJPG'),60, size)
+
+with Boson() as camera:
+    while True:
+        img = camera.grab().astype(np.float32)
+        img = 255*(img - img.min())/(img.max()-img.min())
+        img_col = cv2.applyColorMap(img.astype(np.uint8), cv2.COLORMAP_INFERNO)
+        result.write(img_col)
+        cv2.imshow('Boson',img_col)
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
+video.release()
+result.release()
+cv2.destroyAllWindows()
