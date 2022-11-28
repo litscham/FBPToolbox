7be35b79c4155e51be79bd14090e720954400be9
litscham <108621172+litscham@users.noreply.github.com>
2022-11-28 15:33:31 +0100

    Update live-plot-frame-mean.py
    FPA Temperature Plot and Root mean square line added

--- live-plot-frame-mean.py
+++ live-plot-frame-mean.py
@@ -7,16 +7,22 @@ avg=[]
 x=[]
 fpa_tmp=[]
 
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(2,1)
 
 def animate(i):
     with Boson() as camera:
         frame=camera.grab()
+        fpa_tmp.append(camera.get_fpa_temperature())
         x.append(i)
         avg.append(np.mean(frame))
-        ax.clear()
-        ax.plot(x,avg)
+        rms = np.sqrt(np.mean(np.square(avg)))
+        ax1.clear()
+        ax2.clear()
+        ax1.scatter(x,avg)
+        ax1.hlines(rms,0,len(x),'r')
+        ax1.text(0,rms,'RMS %d'%rms)
+        ax2.plot(x,fpa_tmp)
 
-ani = FuncAnimation(fig, animate, interval=1000)
+ani = FuncAnimation(fig, animate, interval=17)
 
 plt.show()
